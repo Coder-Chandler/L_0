cadccdd7d4b15a4c37a5d74562a1d33cc57a8e89
Coder-Chandler <ysiwgtus@gmail.com>
2017-04-28 00:53:19 +0800

    Binary Tree
    Depth or Breadth searching of Binary Tree , and Binary Search Tree
    searching

---
+++ Algorithm BinaryTree/binarytree.py
@@ -0,0 +1,160 @@
+class binaryTree(object):
+    def __init__(self, value):
+        self.value = value
+        self.leftBranch = None
+        self.rightBranch = None
+        self.parent = None
+
+    def setLeftBranch(self, node):
+        self.leftBranch = node
+
+    def setRightBranch(self, node):
+        self.rightBranch = node
+
+    def setParent(self, parent):
+        self.parent = parent
+
+    def getValue(self):
+        return self.value
+
+    def getLeftBranch(self):
+        return self.leftBranch
+
+    def getRightBranch(self):
+        return self.rightBranch
+
+    def getParent(self):
+        return self.parent
+
+    def __str__(self):
+        return self.value
+
+n5 = binaryTree(5)
+n2 = binaryTree(2)
+n1 = binaryTree(1)
+n4 = binaryTree(4)
+n8 = binaryTree(8)
+n6 = binaryTree(6)
+n7 = binaryTree(7)
+n3 = binaryTree(3)
+
+n5.setLeftBranch(n2)
+n2.setParent(n5)
+n5.setRightBranch(n8)
+n8.setParent(n5)
+n2.setLeftBranch(n1)
+n1.setParent(n2)
+n2.setRightBranch(n4)
+n4.setParent(n2)
+n8.setLeftBranch(n6)
+n6.setParent(n8)
+n6.setRightBranch(n7)
+n7.setParent(n6)
+n4.setLeftBranch(n3)
+n3.setParent(n4)
+
+
+def find6(node):
+    return node.getValue() == 6
+
+
+def find10(node):
+    return node.getValue() == 10
+
+
+def find2(node):
+    return node.getValue() == 2
+
+
+def lt6(node):
+    return node.getValue() > 6
+
+
+#Depth search  time~O(N)
+def DFSBinary(root, fcn):
+    queue = [root]
+    while len(queue) > 0:
+        print 'at node ' + str(queue[0].getValue())
+        if fcn(queue[0]):
+            return True
+        else:
+            temp = queue.pop(0)
+            if temp.getRightBranch():
+                queue.insert(0, temp.getRightBranch())
+            if temp.getLeftBranch():
+                queue.insert(0, temp.getLeftBranch())
+    return False
+
+#Breadth search time~O(n)
+def BFSBinary(root, fcn):
+    queue = [root]
+    while len(queue) > 0:
+        print 'at node ' + str(queue[0].getValue())
+        if fcn(queue[0]):
+            return True
+        else:
+            temp = queue.pop(0)
+            if temp.getLeftBranch():
+                queue.append(temp.getLeftBranch())
+            if temp.getRightBranch():
+                queue.append(temp.getRightBranch())
+    return False
+
+# You can use this function when you know this is a Binary Search Tree.
+# best time~O(logN),worst time~O(N)
+def DFSBinaryOrdered(root, fcn, ltFcn):
+    queue = [root]
+    while len(queue) > 0:
+        if fcn(queue[0]):
+            return True
+        elif ltFcn(queue[0]):
+            temp = queue.pop(0)
+            if temp.getLeftBranch():
+                queue.insert(0, temp.getLeftBranch())
+        else:
+            temp = queue.pop(0)
+            if temp.getRightBranch():
+                queue.insert(0, temp.getRightBranch())
+    return False
+
+
+
+
+# test examples
+
+print 'DFS'
+DFSBinary(n5, find6)
+
+print ''
+print 'BFS'
+BFSBinary(n5, find6)
+
+
+## if we wanted to return the path that got to the goal, would need to modify
+# We can track the search path if we use these two function
+def DFSBinaryPath(root, fcn):
+    queue = [root]
+    while len(queue) > 0:
+        if fcn(queue[0]):
+            return TracePath(queue[0])
+        else:
+            temp = queue.pop(0)
+            if temp.getRightBranch():
+                queue.insert(0, temp.getRightBranch())
+            if temp.getLeftBranch():
+                queue.insert(0, temp.getLeftBranch())
+    return False
+
+
+def TracePath(node):
+    if not node.getParent():
+        return [node]
+    else:
+        return [node] + TracePath(node.getParent())
+
+
+print''
+print 'DFS path'
+pathTo6 = DFSBinaryPath(n5, find6)
+print [e.getValue() for e in pathTo6]
+
