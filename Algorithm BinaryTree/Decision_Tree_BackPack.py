4def40b762def9d8f755df589e627dc2bd7eb980
Coder-Chandler <ysiwgtus@gmail.com>
2017-04-29 00:21:36 +0800

    Decision_Tree_Backpack
    Searching a decision tree

---
+++ Algorithm BinaryTree/Decision_Tree_BackPack.py
@@ -0,0 +1,100 @@
+## make a decision tree
+## for efficiency should really generate on the fly, but here will build
+## and then search
+from binarytree import binaryTree
+#build a decison tree
+def buildDTree(sofar, todo):
+    if len(todo) == 0:
+        return binaryTree(sofar)
+    else:
+        withelt = buildDTree(sofar + [todo[0]], todo[1:])
+        withoutelt = buildDTree(sofar, todo[1:])
+        here = binaryTree(sofar)
+        here.setLeftBranch(withelt)
+        here.setRightBranch(withoutelt)
+        return here
+
+#function helper
+def sumValues(lst):#valueFcn
+    vals = [e[0] for e in lst]
+    return sum(vals)
+
+def sumWeights(lst):
+    wts = [e[1] for e in lst]
+    return sum(wts)
+
+def WeightsBelow10(lst):#constraintFcn
+    return sumWeights(lst) <= 10
+
+def WeightsBelow6(lst):
+    return sumWeights(lst) <= 6
+
+
+
+#Depth searching
+def DFSDTree(root, valueFcn, constraintFcn):
+    stack = [root]
+    best = None
+    visited = 0
+    while len(stack) > 0:
+        visited += 1
+        if constraintFcn(stack[0].getValue()):
+            if best == None:
+                best = stack[0]
+                print best.getValue()
+            elif valueFcn(stack[0].getValue()) > valueFcn(best.getValue()):
+                best = stack[0]
+                print best.getValue()
+            temp = stack.pop(0)
+            if temp.getRightBranch():
+                stack.insert(0, temp.getRightBranch())
+            if temp.getLeftBranch():
+                stack.insert(0, temp.getLeftBranch())
+        else:
+            stack.pop(0)
+    print 'visited', visited
+    return best
+
+#Breadth searching
+def BFSDTree(root, valueFcn, constraintFcn):
+    queue = [root]
+    best = None
+    visited = 0
+    while len(queue) > 0:
+        visited += 1
+        if constraintFcn(queue[0].getValue()):
+            if best == None:
+                best = queue[0]
+                print best.getValue()
+            elif valueFcn(queue[0].getValue()) > valueFcn(best.getValue()):
+                best = queue[0]
+                print best.getValue()
+            temp = queue.pop(0)
+            if temp.getLeftBranch():
+                queue.append(temp.getLeftBranch())
+            if temp.getRightBranch():
+                queue.append(temp.getRightBranch())
+        else:
+            queue.pop(0)
+    print 'visited', visited
+    return best
+
+a = [6,3]
+b = [7,2]
+c = [8,4]
+d = [9,5]
+
+
+treeTest = buildDTree([], [a,b,c,d])
+
+
+if __name__=='__main__':
+    print ''
+    print 'DFS decision tree'
+    foobar = DFSDTree(treeTest, sumValues, WeightsBelow10)
+    print foobar.getValue()
+
+    #print ''
+    #print 'BFS decision tree'
+    #foobarnew = BFSDTree(treeTest, sumValues, WeightsBelow10)
+    #print foobarnew.getValue()
